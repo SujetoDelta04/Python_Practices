0c96ff98360f2f757189ba215d48f01f2597b1c3
Alfredo Melo <alfremelo9@gmail.com>
2024-02-24 15:32:34 +0100

    Se agregaron figuras geometricas al archivo de first_game

--- pygames_practices/first_game.py
+++ pygames_practices/first_game.py
@@ -5,8 +5,23 @@ pygame.init()
 patanlla=pygame.display.set_mode((600, 500))
 tittle=pygame.display.set_caption("Mi primer juego")
 
+#Colores
+Blanco=(255, 255, 255)
+Negro=(0, 0, 0)
+Rojo=(245, 9, 13)
+Azul=(9, 16, 245)
+Verde=(12, 150, 3)
+
+patanlla.fill(Blanco)
+
+rectangulo=pygame.draw.rect(patanlla, Rojo, (300, 300, 200, 150))
+linea=pygame.draw.line(patanlla, Negro, (100, 150), (200, 100), 10)
+circulo=pygame.draw.circle(patanlla, Azul, (400, 200), 40, 0)
+
+
+#Bucle
 while True:
     for event in pygame.event.get():
-        if event.type == quit:
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+    pygame.display.update()
